6b8a13f188dc657556a07aec21133686caf49dcf
Anand Soni <137056068+Anand-Sony@users.noreply.github.com>
2024-10-23 18:07:34 +0530

    Add files via upload

---
+++ OOPs/Inheritance/Employee.java
@@ -0,0 +1,11 @@
+public class Employee {
+
+    public void submitTimesheet() {
+        System.out.println("Submitting TimeSheet");
+    }
+
+    public void bookVacation() {
+        System.out.println("Booking Vacation");
+    }
+
+}
