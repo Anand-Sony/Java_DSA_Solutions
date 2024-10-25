7d78d3706e2dfc62f621de026a9b36914b7848ab
Anand Soni <137056068+Anand-Sony@users.noreply.github.com>
2024-10-25 10:00:43 +0530

    Add files via upload

---
+++ OOPs/Polymorphism/Employee.java
@@ -0,0 +1,15 @@
+public class Employee {
+
+    public void submitTimesheet() {
+        System.out.println("Submitting TimeSheet");
+    }
+
+    public void bookVacation() {
+        System.out.println("Booking Vacation");
+    }
+
+    public void performWork(){
+        System.out.println("Performing Work");
+    }
+
+}
