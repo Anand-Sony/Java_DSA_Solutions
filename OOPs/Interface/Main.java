de34ca57331fcb6dd2b73ff8824841d08495e914
Anand Soni <137056068+Anand-Sony@users.noreply.github.com>
2024-10-29 11:20:18 +0530

    Add files via upload

---
+++ OOPs/Interface/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Drawable s = new Square();
+        Drawable c = new Circle();
+
+        s.draw();
+        c.draw();
+    }
+}
