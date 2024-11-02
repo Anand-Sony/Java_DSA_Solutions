6fdf1fb0411e60caa8b8fc4cdf3c804a96add3a9
Anand Soni <137056068+Anand-Sony@users.noreply.github.com>
2024-11-02 11:08:39 +0530

    Add files via upload

---
+++ OOPs/Aggregation/Person.java
@@ -0,0 +1,36 @@
+public class Person {
+    String name;
+    int age;
+    Address address;
+
+    public Person(String name, int age, Address address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setage(int age) {
+        this.age = age;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+}
