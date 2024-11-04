36c1bf180ae4c9236b8fad8674c42c70b06a76ba
Anand Soni <137056068+Anand-Sony@users.noreply.github.com>
2024-11-04 12:57:11 +0530

    Add files via upload

---
+++ OOPs/Abstraction/Circle.java
@@ -0,0 +1,8 @@
+public class Circle extends Shape {
+    void draw() {
+        System.out.println("Drawing a circle");
+    }
+    public void printDetail(){
+        System.out.println("This is a circle");
+    }
+}
