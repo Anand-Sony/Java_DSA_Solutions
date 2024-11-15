5b00695295c6a8c11f5fdcc548774a698b8e32c9
Anand Soni <137056068+Anand-Sony@users.noreply.github.com>
2024-11-15 14:58:34 +0530

    Add files via upload

---
+++ OOPs/Abstraction/Square.java
@@ -0,0 +1,10 @@
+public class Square extends Shape {
+    void draw() {
+        System.out.println("Drawing Square");
+    }
+
+    public void printDetail() {
+        System.out.println("This is a Square");
+    }
+
+}
